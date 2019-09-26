20ef8a39a6b492f01ae3e19608840d18400e15a8
lminggvick <lminggvick@naver.com>
2019-09-26 15:43:22 +0900

    Initial commit

---
+++ src/test/java/com/abasystem/crawler/AbaCrawlerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.abasystem.crawler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbaCrawlerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        assertTrue(true);
+    }
+}
