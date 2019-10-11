f200fd02af5ca074464e31ff0d4c81455be6d128
lminggvick <lminggvick@naver.com>
2019-10-11 16:35:26 +0900

    Refator: 미사용 parseAll() 메소드 제거

--- src/main/java/com/abasystem/crawler/Service/CrawlerService.java
+++ src/main/java/com/abasystem/crawler/Service/CrawlerService.java
@@ -4,23 +4,16 @@ import com.abasystem.crawler.Factory.ServiceFactory;
 import com.abasystem.crawler.Mapper.ModelMapper;
 import com.abasystem.crawler.Service.Converter.ModelConverter;
 import com.abasystem.crawler.Service.Writer.CustomOpenCsv;
-import com.abasystem.crawler.Storage.Naver;
 import com.abasystem.crawler.Strategy.CsvWriteStrategy;
 import com.abasystem.crawler.Strategy.ValidationStrategy;
 import com.google.gson.JsonObject;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Init 메소드는 Util 성에 성격이 가까워보인다.
@@ -66,34 +59,5 @@ public class CrawlerService<P extends ModelMapper> extends CustomOpenCsv {
         }
 
         return true;
-    }
-
-    public List<P> parseAll(Elements elements, Map<String, String> cookies) throws IOException {
-        properties = new ArrayList<>();
-
-        for (Element post : elements) {
-            url = Naver.CAFE_PREFIX.concat(post.select("a").attr("href"));
-            title = post.select("a").text();
-
-            document = Jsoup.connect(url)
-                    .cookies(cookies)
-                    .get();
-
-            Elements els = document.select(".inbox");
-
-            if(validationStrategy.isInvalidPost(els)) {
-                continue;
-            }
-
-            if ( ! validationStrategy.isPropertyPost(els)) {
-                continue;
-            }
-
-            boolean flag = validationStrategy.isRegularPost(document.select("#tbody"));
-
-            properties.add(factory.parserCreator(flag).parse(document, url, title));
-        }
-
-        return (List<P>) properties;
     }
 }
